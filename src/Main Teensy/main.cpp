d867f878f2cb45036c20728f9baed1737a668f92
DavidLow22 <126950886+DavidLow22@users.noreply.github.com>
2024-01-05 16:08:13 +0800

    -

--- src/Main Teensy/main.cpp
+++ src/Main Teensy/main.cpp
@@ -1,15 +1,15 @@
 #include <Arduino.h>
-
+#include "Wire.h"
+#include "SPI.h"
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Serial.print("Main Teensy");
 }
 
 // put function definitions here:
